43a311a13ae46705cb2a2ed278fa32041ddcf936
Mustafa.Calkan <mustafa-calkan@hotmail.com>
2024-02-20 21:47:49 +0300

    Add project files.

---
+++ FoodyTekmerDataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using FoodyTekmerDataAccessLayer.Abstract;
+using FoodyTekmerDataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodyTekmerDataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        FoodyContext Context = new FoodyContext();
+
+        public void Add(T entity)
+        {
+            Context.Set<T>().Add(entity);
+            Context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var value = Context.Set<T>().Find(id);
+            Context.Set<T>().Remove(value);
+            Context.SaveChanges();
+        }
+
+        public List<T> GetAllList()
+        {
+            return Context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return Context.Set<T>().Find(id);
+        }
+
+        public void Update(T entity)
+        {
+            Context.Set<T>().Update(entity);
+            Context.SaveChanges();
+        }
+    }
+}
---
+++ FoodyTekmerUI/Controllers/CategoryController.cs
@@ -0,0 +1,53 @@
+﻿using FoodyTekmerDataAccessLayer.Context;
+using FoodyTekmerEntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodyTekmerWebUI.Controllers
+{
+	public class CategoryController : Controller
+	{
+		FoodyContext context = new FoodyContext();
+		public IActionResult Index()
+		{
+			var value = context.Categories.ToList();
+			return View(value);
+		}
+		public IActionResult CreateCategory()
+		{
+			return View();
+		}
+		[HttpGet]
+		public IActionResult EditCategory()
+		{
+			return View();
+		}
+		[HttpPost]
+        public IActionResult EditCategory(Category p)
+        {
+			context.Categories.Add(p);
+			context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+		public IActionResult DeleteCategory(int Id)
+		{
+			var value = context.Categories.Find(Id);
+			context.Categories.Remove(value);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		[HttpGet]
+		public IActionResult UpdateCategory(int Id)
+		{
+			var value =context.Categories.Find(Id);
+			return View(value);
+		}
+		[HttpPost]
+		public IActionResult UpdateCategory(Category p)
+		{
+			context.Categories.Update(p);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+    }
+}
